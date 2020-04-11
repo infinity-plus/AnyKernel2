b7a02ef9b46385435859dbf565b5525af44363e8
infinity-plus <navdhareharsh2001@gmail.com>
2020-04-11 10:02:30 +0530

    X00T specific changes
    Signed-off-by: infinity-plus <navdhareharsh2001@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,23 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
+kernel.string=WolfKernel by  infinitEplus
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-device.name2=
-device.name3=
-device.name4=
-device.name5=
+device.name1=ASUS_X00TD
+device.name2=ASUS_X00T
+device.name3=ASUS_X00TDA
+device.name4=X00TD
+device.name5=X00TA
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -39,7 +39,35 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 ## AnyKernel install
 dump_boot;
 
+# Backup init.rc
+backup_file init.rc;
+
 # begin ramdisk changes
+
+# Overdose's Ramdisk Removal & Injection
+rm -rf $ramdisk/init.trb.rc;
+rm -rf $ramdisk/init.azure.rc;
+rm -rf $ramdisk/init.Pbh.rc;
+rm -rf $ramdisk/init.PbH.rc;
+rm -rf $ramdisk/init.darkonah.rc;
+rm -rf $ramdisk/init.spectrum.rc;
+rm -rf $ramdisk/init.spectrum.sh;
+rm -rf $ramdisk/init.special_power.sh;
+rm -rf $ramdisk/init.thundercloud.rc;
+rm -rf $ramdisk/init.thundercloud.sh;
+
+remove_line init.rc "import /init.trb.rc"
+remove_line init.rc "import /init.azure.rc"
+remove_line init.rc "import /init.PbH.rc"
+remove_line init.rc "import /init.Pbh.rc"
+remove_line init.rc "import /init.darkonah.rc"
+remove_line init.rc "import /init.overdose.rc"
+remove_line init.rc "import /init.thundercloud.rc"
+remove_line init.rc "import /init.spectrum.rc"
+remove_line init.rc "import /init.pk.rc"
+remove_line init.rc "import /init.boost.rc"
+remove_line init.rc "import /init.stardust.rc"
+insert_line init.rc "import /init.overdose.rc" after "import /init.usb.configfs.rc" "import /init.overdose.rc";
 
 
 # end ramdisk changes
